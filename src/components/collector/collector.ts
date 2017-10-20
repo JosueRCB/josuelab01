48b6cb1e321b3ca3aacfbcf96b598e65bd43e27b
Mark van den Brink <mark@vandenbr.ink>
2017-10-20 16:34:48 +0200

    Finalize bootstrap template

--- src/components/collector/collector.ts
+++ src/components/collector/collector.ts
@@ -9,15 +9,23 @@ import * as Tripetto from '@tripetto/forms-collector';
 })
 export class CollectorComponent implements OnInit {
   collector: Tripetto.Collector<{}>;
+  resumeData: Tripetto.ISnapshot | undefined;
 
   constructor(private _http: HttpClient) {}
 
   ngOnInit() {
     this._http.get('/assets/form.json').subscribe((data: Tripetto.IMap) => {
-      this.collector = new Tripetto.Collector<{}>(data);
+      this.collector = new Tripetto.Collector<{}>(data, false, undefined, this.end.bind(this));
 
       this.collector.Start();
     });
+  }
+
+  private end(instance: Tripetto.Instance, type: 'ended' | 'stopped' | 'paused'): void {
+    if (type === 'ended') {
+      // Output the collected data to the console
+      console.dir(instance.Data.Values);
+    }
   }
 
   private clickPrevious(): void {
@@ -30,5 +38,26 @@ export class CollectorComponent implements OnInit {
     if (this.collector.Observer) {
       this.collector.Observer.Done();
     }
+  }
+
+  private clickStart(): void {
+    this.collector.Start();
+  }
+
+  private clickPause(): void {
+    this.resumeData = this.collector.Pause();
+
+    // Output the resume data to the console
+    console.dir(this.resumeData);
+  }
+
+  private clickResume(): void {
+    if (this.resumeData) {
+      this.collector.Resume(this.resumeData);
+    }
+  }
+
+  private clickStop(): void {
+    this.collector.Stop();
   }
 }
