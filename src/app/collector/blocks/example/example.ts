075adee73f137d07bd50f003a7d7ddb4be1de5d7
Mark van den Brink <mark@vandenbr.ink>
2018-12-14 09:32:31 +0100

    Improve example block

--- src/app/collector/blocks/example/example.ts
+++ src/app/collector/blocks/example/example.ts
@@ -1,4 +1,4 @@
-import { NodeBlock, validator } from 'tripetto-collector';
+import * as Tripetto from 'tripetto-collector';
 import { Block, BlockComponentFactory } from '../../helpers/blocks/block.factory';
 import { Component } from '@angular/core';
 
@@ -13,15 +13,23 @@ import { Component } from '@angular/core';
     return BlockComponent;
   }
 })
-export class CheckboxBlock extends NodeBlock {
-  @validator
+export class CheckboxBlock extends Tripetto.NodeBlock {
+  get exampleValue(): Tripetto.Value<string> {
+    return Tripetto.assert(this.value('example-slot'));
+  }
+
+  @Tripetto.validator
   validate(): boolean {
     const exampleSlot = this.slot('example-slot');
+
+    if (!exampleSlot) {
+      return false;
+    }
 
     if (exampleSlot.required) {
       const exampleValue = this.value(exampleSlot);
 
-      return exampleValue && exampleValue.hasValue;
+      return (exampleValue && exampleValue.hasValue) || false;
     }
 
     return true;
