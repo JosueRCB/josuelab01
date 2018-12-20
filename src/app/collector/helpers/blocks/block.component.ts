7d8ba6e07cffe79defe8261c57b59a2b07ecdd1c
Mark van den Brink <mark@vandenbr.ink>
2018-12-20 11:40:57 +0100

    Make sure blocks run in Angular zone

--- src/app/collector/helpers/blocks/block.component.ts
+++ src/app/collector/helpers/blocks/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewContainerRef, ComponentFactoryResolver, Type } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ComponentFactoryResolver, Type, NgZone } from '@angular/core';
 import { BlockComponentFactory } from './factory';
 
 @Component({
@@ -6,17 +6,25 @@ import { BlockComponentFactory } from './factory';
   templateUrl: './block.component.html'
 })
 export class BlockComponent extends BlockComponentFactory implements OnInit {
-  constructor(private viewContainerRef: ViewContainerRef, private componentFactoryResolver: ComponentFactoryResolver) {
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private zone: NgZone
+  ) {
     super();
   }
 
   ngOnInit() {
     if (this.node.block) {
-      const component = this.componentFactoryResolver.resolveComponentFactory<BlockComponentFactory>(this.node.block.type.ref as Type<any>);
-      const instance = this.viewContainerRef.createComponent<BlockComponentFactory>(component).instance;
+      this.zone.run(() => {
+        const component = this.componentFactoryResolver.resolveComponentFactory<BlockComponentFactory>(this.node.block.type.ref as Type<
+          any
+        >);
+        const instance = this.viewContainerRef.createComponent<BlockComponentFactory>(component).instance;
 
-      instance.node = this.node;
-      instance.collector = this.collector;
+        instance.node = this.node;
+        instance.collector = this.collector;
+      });
     }
   }
 }
